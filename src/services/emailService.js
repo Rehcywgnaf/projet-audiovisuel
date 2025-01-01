1f3d2bf28d78d7d890ee196005f71c003fa21b95
Rehcywgnaf <137691949+Rehcywgnaf@users.noreply.github.com>
2025-01-01 18:50:41 +0100

    feat: Intégration système de veille RSS et alertes

---
+++ src/services/emailService.js
@@ -0,0 +1,7 @@
+// Service d'alertes email
+class EmailAlertService {
+  constructor() {
+    this.subscribers = new Map();
+  }
+
+  // Rest of the service code...
---
+++ src/services/rssService.js
@@ -0,0 +1,8 @@
+// Service de gestion RSS
+class RSSService {
+  constructor() {
+    this.sources = {};
+    this.lastUpdate = null;
+  }
+
+  // Rest of the service code...
