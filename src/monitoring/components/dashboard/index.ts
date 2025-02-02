53cba8ea7e1424f9dc69a588198dd79255848b70
Rehcywgnaf <137691949+Rehcywgnaf@users.noreply.github.com>
2025-02-02 14:26:56 +0100

    feat(monitoring): Create dashboard components index

---
+++ src/monitoring/components/dashboard/index.ts
@@ -0,0 +1,4 @@
+export { default as MonitoringDashboard } from './MonitoringDashboard';
+export { default as AIPerformanceView } from './AIPerformanceView';
+export { default as ProjectMetricsView } from './ProjectMetricsView';
+export { default as TeamMetricsView } from './TeamMetricsView';
