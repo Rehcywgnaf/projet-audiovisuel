7e66193b1786812a26420e712dfc9458a8b4361f
Rehcywgnaf <137691949+Rehcywgnaf@users.noreply.github.com>
2025-02-02 10:20:14 +0100

    update(monitoring): Add long term metrics manager to exports

--- src/monitoring/index.ts
+++ src/monitoring/index.ts
@@ -1,6 +1,7 @@
 // Core exports
 export { BaseMetricsManager } from './core/metrics/baseMetrics';
 export { QueueMetricsManager } from './core/metrics/queueMetrics';
+export { LongTermMetricsManager } from './core/metrics/longTermMetrics';
 export { MonitoringService } from './core/MonitoringService';
 
 // Types exports
@@ -10,3 +11,4 @@ export * from './types/queue.types';
 // Singleton instance exports
 export const monitoringService = MonitoringService.getInstance();
 export const queueMetrics = QueueMetricsManager.getInstance();
+export const longTermMetrics = LongTermMetricsManager.getInstance();
