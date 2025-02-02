7ba24d2705244a36718cc40cc4851d1f77e1a719
Rehcywgnaf <137691949+Rehcywgnaf@users.noreply.github.com>
2025-02-02 10:19:13 +0100

    update(monitoring): Add MonitoringService to index exports

--- src/monitoring/index.ts
+++ src/monitoring/index.ts
@@ -1,10 +1,12 @@
 // Core exports
 export { BaseMetricsManager } from './core/metrics/baseMetrics';
 export { QueueMetricsManager } from './core/metrics/queueMetrics';
+export { MonitoringService } from './core/MonitoringService';
 
 // Types exports
 export * from './types/metrics.types';
 export * from './types/queue.types';
 
 // Singleton instance exports
+export const monitoringService = MonitoringService.getInstance();
 export const queueMetrics = QueueMetricsManager.getInstance();
