f6644a7dc8a0d5ac754c19c1fdd4c62c9d0323d4
Rehcywgnaf <137691949+Rehcywgnaf@users.noreply.github.com>
2025-02-04 15:06:35 +0100

    SAPAV: Ajout export DriveCore

--- src/components/Drive/Core/index.ts
+++ src/components/Drive/Core/index.ts
@@ -1,3 +1,3 @@
 export { default as DriveCore } from './DriveCore';
-export { default as DriveSync } from './DriveSync';
-export { default as DrivePerms } from './DrivePerms';
+export * from './DriveConfig';
+export * from './DriveSync';
