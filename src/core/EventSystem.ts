8e0e67f23fc132d7681354fc128d1729bda82604
Rehcywgnaf <137691949+Rehcywgnaf@users.noreply.github.com>
2025-02-04 20:56:21 +0100

    feat: ajout EventSystem

--- src/core/EventSystem.ts
+++ src/core/EventSystem.ts
@@ -1,41 +1,51 @@
 type EventCallback = (data: any) => void;
-type EventName = 'roleChanged' | 'permissionChanged';
 
-class EventSystem {
-    private static instance: EventSystem;
-    private listeners: Map<EventName, EventCallback[]>;
+export class EventSystem {
+  private static instance: EventSystem;
+  private listeners: Map<string, EventCallback[]>;
 
-    private constructor() {
-        this.listeners = new Map();
+  private constructor() {
+    this.listeners = new Map();
+  }
+
+  static getInstance(): EventSystem {
+    if (!EventSystem.instance) {
+      EventSystem.instance = new EventSystem();
     }
+    return EventSystem.instance;
+  }
 
-    static getInstance(): EventSystem {
-        if (!EventSystem.instance) {
-            EventSystem.instance = new EventSystem();
-        }
-        return EventSystem.instance;
+  emit(event: string, data: any): void {
+    const callbacks = this.listeners.get(event);
+    if (callbacks) {
+      callbacks.forEach(callback => callback(data));
     }
+  }
 
-    on(event: EventName, callback: EventCallback) {
-        if (!this.listeners.has(event)) {
-            this.listeners.set(event, []);
-        }
-        this.listeners.get(event)?.push(callback);
+  on(event: string, callback: EventCallback): void {
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, []);
     }
+    this.listeners.get(event)?.push(callback);
+  }
 
-    emit(event: EventName, data: any) {
-        if (this.listeners.has(event)) {
-            this.listeners.get(event)?.forEach(callback => callback(data));
-        }
+  off(event: string, callback: EventCallback): void {
+    const callbacks = this.listeners.get(event);
+    if (callbacks) {
+      const index = callbacks.indexOf(callback);
+      if (index > -1) {
+        callbacks.splice(index, 1);
+      }
     }
+  }
 
-    off(event: EventName, callback: EventCallback) {
-        if (this.listeners.has(event)) {
-            const callbacks = this.listeners.get(event) ?? [];
-            const index = callbacks.indexOf(callback);
-            if (index > -1) {
-                callbacks.splice(index, 1);
-            }
-        }
-    }
+  clearAllListeners(): void {
+    this.listeners.clear();
+  }
+
+  getListenerCount(event: string): number {
+    return this.listeners.get(event)?.length ?? 0;
+  }
 }
+
+export default EventSystem;
