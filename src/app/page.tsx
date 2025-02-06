cf6d68dc2b3e38e55fba6e230b357558e37838f3
Rehcywgnaf <137691949+Rehcywgnaf@users.noreply.github.com>
2025-02-06 09:00:21 +0100

    refactor: utilisation des composants clients pour le Drive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 'use client';
 
-import DriveAuth from '@/components/Drive/Auth/DriveAuth';
-import { DriveAuthProvider } from '@/components/Drive/Auth/DriveAuthProvider';
-import DriveIntegration from '@/components/Drive/Integration/DriveIntegration';
+import DriveAuthPage from './drive/auth/page';
+import DriveProvider from './drive/provider/page';
+import DriveIntegrationPage from './drive/integration/page';
 import React from 'react';
 
 export default function Home() {
   return (
     <main className="p-4 space-y-8">
-      <DriveAuthProvider>
-        <DriveAuth />
-        <DriveIntegration />
-      </DriveAuthProvider>
+      <DriveProvider>
+        <DriveAuthPage />
+        <DriveIntegrationPage />
+      </DriveProvider>
     </main>
   );
 }
